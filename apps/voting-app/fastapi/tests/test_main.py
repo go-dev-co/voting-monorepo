8e5a3cd70231191adda267c3c964df941c7da9f2
MalevolentThesis <109775964+MalevolentThesis@users.noreply.github.com>
2023-08-11 20:01:54 -0500

    fixed test issues

---
+++ apps/voting-app/fastapi/tests/test_main.py
@@ -0,0 +1,14 @@
+
+import pytest
+
+from fastapi.testclient import TestClient
+from src.main import app
+
+
+client = TestClient(app)
+
+
+def test_get_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to FastAPI"}
